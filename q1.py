c314b9d9ed031d88ed2b8f142c5818b4d488675a
oikn2018 <oikantiknath@gmail.com>
2023-02-19 18:34:56 +0530

    Importing libraries for Q1

--- q1.py
+++ q1.py
@@ -1,1 +1,15 @@
-# Testing
+import wandb
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+
+from keras.datasets import fashion_mnist
+
+
+wandb.init(project="CS6910 Assignment 1", name="Assignment 1 Question 1")
+
+classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
