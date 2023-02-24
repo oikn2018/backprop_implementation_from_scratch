727114726e98d6f8bf8bc67a0445a39f77bfd363
oikn2018 <oikantiknath@gmail.com>
2023-02-24 23:52:14 +0530

    Added first Question

--- q1.py
+++ q1.py
@@ -1,5 +1,5 @@
 import wandb
-
+# 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,3 +13,26 @@ from keras.datasets import fashion_mnist
 wandb.init(project="CS6910 Assignment 1", name="Assignment 1 Question 1")
 
 classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+(x, y), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Number of features in the dataset
+num_features = 784
+
+# Number of classes
+num_classes = len(set(y_train))
+
+# sample_indices contain index of first occurence image of each class
+sample_indices = [list(y_train).index(i) for i in range(num_classes)]
+
+sample_images = []
+sample_labels = []
+
+for index in sample_indices:
+    sample_images.append(x_train[index])
+    sample_labels.append(classes[y_train[index]])
+
+wandb.log({"Sample Image from each class": [wandb.Image(image, caption=label) for image, label in zip(sample_images, sample_labels)]})
+
